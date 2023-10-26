aee41bfe9e53a65898d1a87c80eda119b4cf8d55
MsDiala <diala-is-diala@hotmail.com>
2023-10-26 18:43:07 +0100

    done

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+// Declare customerName to be 'bob' in global scope
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = "Alice";
+
+function changeLeastFavoriteCustomer() {
+  // This will throw an error because you cannot reassign a constant.
+  leastFavoriteCustomer = "Bob";
+}
+
+// module.exports = {
+//   customerName,
+//   upperCaseCustomerName,
+//   setBestCustomer,
+//   overwriteBestCustomer,
+//   leastFavoriteCustomer,
+//   changeLeastFavoriteCustomer,
+// };
